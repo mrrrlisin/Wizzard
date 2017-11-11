1223bbed52ec9d840aae90a1917e324f2a988e4f
Михаил Иванов <wowivan2011@yandex.ru>
2017-11-11 12:53:43 +0300

    first

---
+++ Wizzard/Assets/Scripts/Story.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Story : MonoBehaviour {
+    
+    enum States
+    {
+        kidnap,
+        death,
+        totrture,
+        badEnd,
+        goodEnd
+    }
+    States state = States.kidnap;
+    public Text mainText; 
+    void Update ()
+    {
+		
+	}
+
+    void Kidnap()
+    {
+        mainText.text = "Тебя схватили и закинули в багажник\n" + 
+            "(П)Попытаться сбежать\n (Н)Ничего не делать";
+
+    }
+}
